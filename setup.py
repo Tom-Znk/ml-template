2eb7b950ed9981790be03ae7f466e384c8af6a4f
Tom-Znk <tomasz@unifysquare.com>
2023-10-01 17:49:35 +0100

    added a setup.py

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='template', #change project name
+    version='0.1.0',
+    description='A short description of your project',
+    author='Tomas Zinkevic',
+    author_email='tom.zinkevic@gmail.com',
+    url='https://github.com/Tom-Znk/template', #change project name
+    packages=find_packages(),
+    install_requires=[
+        'numpy',             
+        'scikit-learn',
+        'torch',
+        'matplotlib',
+        'tqdm',
+        # Add more dependencies as needed
+    ],
+    python_requires='>=3.11',  # Specify the minimum Python version required
+)
